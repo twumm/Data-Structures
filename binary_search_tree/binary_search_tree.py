762152397eec6f9e10bdfbf24e8d79818999e27c
Martin Twum Mensah <martint.mensah@gmail.com>
2020-02-26 14:37:53 +0000

    chore: Attempt insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,8 @@
+from dll_queue import Queue
+from dll_stack import Stack
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../queue_and_stack')
-from dll_stack import Stack
-from dll_queue import Queue
 
 
 class BinarySearchTree:
@@ -9,18 +10,56 @@ class BinarySearchTree:
         self.value = value
         self.left = None
         self.right = None
+        self.size = 0
+        self.storage = DoublyLinkedList()
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # LEFT CASE
+        # check if our new node's value is less than the current node's value
+        if value < self.value:
+            # does it have a child to the left?
+            if not self.left:
+                # place our new node here
+                self.storage.add_to_head(value)
+            # otherwise
+            else:
+                # repeat process for the left
+                BinarySearchTree.insert(self, value)
+
+        # RIGHT CASE
+        # check if the new nodes value is greater than or equal to the current nodes value
+        if value >= self.value:
+            # does it have a child to the right?
+            if not self.right:
+                # place our new node here
+                self.storage.add_to_tail(value)
+        # otherwise
+        else:
+            # repeat the process for the right
+            BinarySearchTree.insert(self, value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # BASE CASE
+
+        # LEFT CASE
+
+        # RIGHT CASE
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # base case
+        # if empty tree
+            # return none
+
+        # recursive case
+        # if there is no right value 
+            # return the root node value
+        # otherwise
+            # return get max of the right hand child
         pass
 
     # Call the function `cb` on the value of each node
